1e7c763d42411029d803d91bdb6316eff9e1b2df
Lokesh Rana <gokuhunt.dbz@gmail.com>
2022-01-10 12:47:36 +0530

    Fixing the about pages and routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import './App.css';
 import React, {useState} from 'react'
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    // Link
+  } from "react-router-dom";
 
 function App() {
-    const [mode, setMode] = useState("dark")
+    const [mode, setMode] = useState("light")
+    const [alert, setAlert] = useState(null)
+    const showAlert =(message, type)=>{
+        setAlert(
+           { msg: message,
+           type: type}
+        )
+
+        setTimeout(() => {
+            setAlert(null)
+        }, 1500);
+    }
     const toggleMode= ()=> {
         if (mode === "dark"){
             setMode("light")
             document.body.style.backgroundColor = 'white'
+            showAlert("Light mode is enabled", "success");
+            document.title = "TextUtils- Light Mode"
         }
         else{
             setMode("dark")
             document.body.style.backgroundColor = '#042743'
+            showAlert("Dark mode is enabled", "success");
+            document.title = "TextUtils- Light Mode"
         }
     }
+    
     return (<>
+        <Router>
         <Navbar title="TextUtils" aboutText="About Us" toggleMode= {toggleMode} mode= {mode}/>
+        <Alert alert={alert}/>
         <div className="container my-3">
-        <TextForm heading ="Enter the text to analyse below" mode = {mode}/>
-        {/* <About/> */}
+        <Switch>
+          {/* use exact to match the url exactly otherwise it will match partailly */}
+          <Route exact path="/about">
+            <About mode= {mode}/>
+          </Route>
+          <Route exact path="/">
+            <TextForm heading ="Enter the text to analyse below" mode = {mode} showAlert= {showAlert}/>
+          </Route>
+        </Switch>
         </div>
+        </Router>
         
     </>);
 }
